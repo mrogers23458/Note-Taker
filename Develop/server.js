2790e7c2f5b6681ceadfe20a434e0a04ad562f1d
Mike Rogers <mrogers23458@gmail.com>
2021-09-09 09:26:28 -0700

    added get route for root to return index.html

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const notesDB = require('./db/db.json')
 const fs = require ('fs')
 const util = require('util')
@@ -7,3 +8,14 @@ const { readFromFile,
         readAndAppend,
         writeToFile, } = require ('./helpers/fsUtils')
 const uuId = require('./helpers/uuid')
+const PORT = process.env.port || 3001;
+
+
+//Middleware to parse JSON
+app.use(express.json)
+app.use(express.static('public'))
+
+//Get route for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
