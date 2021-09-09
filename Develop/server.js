27ea85f8dc11180dabdf010517534a3777590713
Mike Rogers <mrogers23458@gmail.com>
2021-09-09 09:30:44 -0700

    added get route for /notes to return notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -26,3 +26,9 @@ app.get('*', (req, res) => {
     console.info(`${req.method} received`)
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
+
+//get route for notes page
+app.get('/notes', (req, res) => {
+    console.info(`${req.method} received`)
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
