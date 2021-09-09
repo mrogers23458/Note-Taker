14d11a19c0c7c72c1842827959ad6816b72c6503
Mike Rogers <mrogers23458@gmail.com>
2021-09-09 10:03:59 -0700

    installed new dependencies, re-organized and tested get routes, all routes working as intended

--- Develop/server.js
+++ Develop/server.js
@@ -8,31 +8,30 @@ const { readFromFile,
         readAndAppend,
         writeToFile, } = require ('./helpers/fsUtils')
 const uuId = require('./helpers/uuid')
-const PORT = process.env.port || 3001;
+const PORT = 3001;
 
 
 //Middleware to parse JSON
-app.use(express.json)
+app.use(express.json())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
 
 //Get route for homepage
 app.get('/', (req, res) => {
-    console.info(`${req.method} received`)
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-})
-
-//Get route for *
-app.get('*', (req, res) => {
-    console.info(`${req.method} received`)
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 //get route for notes page
 app.get('/notes', (req, res) => {
-    console.info(`${req.method} received`)
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
+//Get route for *
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+
 //listening on port
 app.listen(PORT, () =>
-console.log(`app listening at http://localhost:${PORT}!!!`))
+console.log(`app listening at http://localhost:${PORT}`))
